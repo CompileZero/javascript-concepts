4c9d13cd9644fe3852e86c84516316ea61463a7f
Atharva <atharvaak66@gmail.com>
2022-01-14 16:39:02 +0530

    added window and this js file

---
+++ window_this.js
@@ -0,0 +1,3 @@
+// In the global Execution Context, this points to the window object.
+// Window object is a global object that is created by a javascript engine when the program begins execution
+console.log(this);
